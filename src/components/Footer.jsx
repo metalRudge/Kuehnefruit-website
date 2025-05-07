6dfac49d48d710aa96e5e3e7a4bc7a950dd70296
metalrudge <stefankprog@outlook.com>
2025-05-07 10:08:00 +0200

    final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import '../styles/Footer.css';
+function Footer()
+{
+  return (<div id='root'>
+    <footer className="footer">
+      <p>&copy; 2024 KUEHNEFRUIT. All Rights Reserved.</p>
+    </footer>
+</div>
+    
+  );
+};
+export default Footer;
---
+++ src/hooks/useViewPortWidth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function useViewportWidth() {
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setViewportWidth(window.innerWidth);
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return { viewportWidth};
+}
+
+export default useViewportWidth;
